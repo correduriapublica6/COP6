7c91884b136bbc8ca014839e5cb2da939a76a5fd
correduriapublica6 <soportecorreduriapublica6@gmail.com>
2026-09-23 17:25:46 -0600

    Add files via upload

--- desktop/sw.js
+++ desktop/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "control-operativo-cp6-v34-kilometraje-navbar-centro";
+const CACHE_NAME = "control-operativo-cp6-v35-gestion-acciones-iconos";
 const APP_SHELL = ["./", "./index.html", "./styles.css", "./dashboard.css", "./override.css", "./app.js", "./automotriz-form.js", "./automotriz-model.js", "./mobiliario-form.js", "./solicitudes-form.js", "./image-utils.js", "./word-export.js", "./vendor/html-docx.js", "./manifest.webmanifest", "./assets/fondo.jpg", "./assets/pwa-icon-512.png"];
 
 self.addEventListener("install", (event) => {
